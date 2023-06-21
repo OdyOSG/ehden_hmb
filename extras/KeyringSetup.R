303b2d1120be50d4cbf14a3eb0f89e2ee5d8d83d
Martin Lavallee <44976592+mdlavallee92@users.noreply.github.com>
2023-06-21 14:51:20 -0400

    update keyring setup

--- extras/KeyringSetup.R
+++ extras/KeyringSetup.R
@@ -1,9 +1,31 @@
-# Keyring Setup -------------
+# Setup Credentials -------------
+# This file setups the credential library for your study. The function establishes
+# a config.yml file and creates a keyring for the study. Input your credentials
+# into the keyring.
 
-configBlock <- "[Place configblock name]" # name of config block
+## A) Depedendencies ------------
 
-keyringName <- "ehden_hmb"
-keyringPassword <- "" # This password is simply to avoid a prompt when creating the keyring
+library(tidyverse, quietly = TRUE)
+library(Ulysses)
+library(keyring)
+
+## B) Set Parameters ------------
+
+configBlock <- "odysseus" # name of config block
+
+database <- "synpuf_110k" # the name of the database in the config block
+
+keyringName <- "ehden_hmb" # the name of the keyring
+
+keyringPassword <- "ulysses" # password for keyring
+# This password is simply to avoid a prompt when creating the keyring
+
+## c) Create Config File------------------------
+
+# create the config.yml file
+makeConfig(block = configBlock, database = database)
+
+## D) Setup Keyring -----------------
 
 # Create the keyring if it does not exist.
 # If it exists, clear it out so we can re-load the keys
@@ -26,21 +48,52 @@ if (keyringName %in% allKeyrings$keyring) {
 # set a new keyring for study
 keyring::keyring_create(keyring = keyringName, password = keyringPassword)
 
-
+## E) Set Credentials -----------------------
 creds <- c(
   "dbms", # the database dialect
   "user", # the user name for the db
   "password", # the password for the db
   "connectionString", # the connection string to access the db
-  "cdmDatabase", # the database hosting the cdm
-  "cdmSchema", # the schema hosting the cdme
-  "vocabSchema", # the schema hosting the vocabulary, usually same as cdm
-  "workDatabase", # the database hosting the work or scratch
-  "workSchema", # the schema hosting the work or scratch
+  "cdmDatabaseSchema", # the database + schema (or just schema) hosting the cdm
+  "vocabDatabaseSchema", # the database + schema (or just schema) hosting the vocabulary, usually same as cdm
+  "workDatabaseSchema", # the database + schema (or just schema) hosting the work or scratch
   "role" # the role id for your user in the db
 )
 
 cred_block <- paste(configBlock, creds, sep = "_")
 
 # set a new keyring for study
+# this will lead to a dialog box enter the credentials
 purrr::walk2(cred_block, creds, ~keyring::key_set(service = .x, keyring = keyringName, prompt = .y))
+
+
+## F) Check (Optional) -----------------------
+
+
+### Review that the credentials are correct
+
+blurCreds <- function(item, configBlock, keyringName) {
+  credential <- paste(configBlock, item, sep = "_")
+  cred <- keyring::key_get(service = credential, keyring = keyringName)
+  txt <- glue::glue(item, ": ", crayon::blurred(cred))
+  cli::cat_bullet(txt, bullet = "info", bullet_col = "blue")
+  invisible(credential)
+}
+
+purrr::walk(creds, ~blurCreds(item = .x, configBlock = configBlock, keyringName = keyringName))
+
+
+### Test connection details
+connectionDetails <- DatabaseConnector::createConnectionDetails(
+  dbms = config::get("dbms", config = configBlock),
+  user = config::get("user", config = configBlock),
+  password = config::get("user", config = configBlock),
+  connectionString = config::get("connectionString", config = configBlock)
+)
+connectionDetails$dbms
+
+
+# G) Session Info ------------------------
+
+sessioninfo::session_info()
+rm(list=ls())
