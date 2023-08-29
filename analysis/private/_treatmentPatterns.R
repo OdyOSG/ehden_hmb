9253a55be08e605a342b70c4da6a46225c3d850a
Martin Lavallee <44976592+mdlavallee92@users.noreply.github.com>
2023-08-29 15:20:31 -0400

    add post index prevalence

--- analysis/private/_treatmentPatterns.R
+++ analysis/private/_treatmentPatterns.R
@@ -7,6 +7,189 @@
 
 # B. Functions ------------------------
 
+## Post index Prevalence -------------------
+
+# Cohort Covariates -----------
+
+verboseSave <- function(object, saveName, saveLocation) {
+
+  savePath <- fs::path(saveLocation, saveName, ext = "csv")
+  readr::write_csv(object, file = savePath)
+  cli::cat_line()
+  cli::cat_bullet("Saved file ", crayon::green(basename(savePath)), " to:",
+                  bullet = "info", bullet_col = "blue")
+  cli::cat_bullet(crayon::cyan(saveLocation), bullet = "pointer", bullet_col = "yellow")
+  cli::cat_line()
+  invisible(savePath)
+}
+
+cohortCovariates <- function(con,
+                             cohortDatabaseSchema,
+                             cohortTable,
+                             cohortKey,
+                             covariateKey,
+                             timeA,
+                             timeB,
+                             outputFolder) {
+
+  cli::cat_rule("Build Cohort Covariates")
+
+  targetId <- cohortKey$id
+  eventId <- covariateKey$id
+  #sql to get cohort covariates - period prevalence change
+  sql <- "
+    SELECT
+      t.cohort_definition_id AS target_cohort_id,
+      e.cohort_definition_id AS covariate_cohort_id,
+      count(e.subject_id) as n
+    FROM (
+      SELECT *
+      FROM @cohortDatabaseSchema.@cohortTable
+      WHERE cohort_definition_id IN (@targetId)
+    ) t
+    JOIN (
+      SELECT *
+      FROM @cohortDatabaseSchema.@cohortTable
+      WHERE cohort_definition_id IN (@eventId)
+    ) e
+    ON t.subject_id = e.subject_id
+    AND (
+      e.cohort_start_date BETWEEN
+          DATEADD(day, @timeA, t.cohort_start_date) AND
+          DATEADD(day, @timeB, t.cohort_start_date)
+      OR e.cohort_end_date BETWEEN
+          DATEADD(day, @timeA, t.cohort_start_date) AND
+          DATEADD(day, @timeB, t.cohort_start_date))
+    GROUP BY t.cohort_definition_id, e.cohort_definition_id
+"
+  # Render and translate sql
+  cohortCovariateSql <- SqlRender::render(
+    sql,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable,
+    targetId = targetId,
+    eventId = eventId,
+    timeA = timeA,
+    timeB = timeB
+  ) %>%
+    SqlRender::translate(targetDialect = con@dbms)
+
+  #run query on connection
+  tb <- dplyr::tbl(con, dbplyr::in_schema(cohortDatabaseSchema, cohortTable)) %>%
+    dplyr::filter(cohort_definition_id %in% targetId) %>%
+    dplyr::count(cohort_definition_id) %>%
+    dplyr::collect() %>%
+    dplyr::right_join(cohortKey, by = c("cohort_definition_id" = "id")) %>%
+    dplyr::rename(id = cohort_definition_id) %>%
+    dplyr::select(id, name, n) %>%
+    dplyr::arrange(id)
+
+
+  #run query on connection
+  cohortCovTbl <- DatabaseConnector::querySql(con, sql = cohortCovariateSql) %>%
+    rename(
+      cohortId = TARGET_COHORT_ID,
+      covariateId = COVARIATE_COHORT_ID,
+      count = N
+    ) %>%
+    dplyr::left_join(
+      tb, by = c("cohortId" = "id")
+    ) %>%
+    dplyr::rename(cohortName = name) %>%
+    dplyr::left_join(
+      covariateKey, by = c("covariateId" = "id")
+    ) %>%
+    dplyr::rename(covariateName = name) %>%
+    dplyr::mutate(
+      pct = count / n
+    ) %>%
+    dplyr::select(
+      cohortId, cohortName, covariateId, covariateName, count, pct
+    )
+
+  verboseSave(
+    object = cohortCovTbl,
+    saveName = paste("cohort_covariates", abs(timeA), abs(timeB), sep = "_"),
+    saveLocation = outputFolder
+  )
+
+
+  invisible(cohortCovTbl)
+
+
+}
+
+executePostIndexDrugUtilization <- function(con,
+                                            executionSettings,
+                                            analysisSettings) {
+
+  ## Prep
+  ## get schema vars
+  cdmDatabaseSchema <- executionSettings$cdmDatabaseSchema
+  workDatabaseSchema <- executionSettings$workDatabaseSchema
+  cohortTable <- executionSettings$cohortTable
+  databaseId <- executionSettings$databaseName
+
+  outputFolder <- fs::path(here::here("results"), databaseId, analysisSettings[[1]]$outputFolder) %>%
+    fs::dir_create()
+  piduFolder <- outputFolder[[1]]
+
+  ## get cohort Ids
+  cohortKey <- analysisSettings$treatmentPatterns$cohorts$targetCohort
+  covariateKey <- analysisSettings$treatmentPatterns$cohorts$drugCohorts
+
+  timeA <- analysisSettings$treatmentPatterns$timeWindow$startDays
+  timeB <- analysisSettings$treatmentPatterns$timeWindow$endDays
+
+  #set ids
+  cohortId <- cohortKey$id
+  covId <- covariateKey$id
+
+
+  #Start execution talk
+  cli::cat_boxx("Building Post-Index Covariates")
+  cli::cat_line()
+
+  tik <- Sys.time()
+  #loop on time Windows
+  for (i in seq_along(timeA)) {
+    # Job Log
+    cli::cat_rule(glue::glue("Post-Index Analysis Job ", i))
+    cli::cat_bullet("Running Post-Index Analysis at window: [",
+                    crayon::green(timeA[i]), " - ",
+                    crayon::green(timeB[i]), "]",
+                    bullet = "info", bullet_col = "blue")
+    cat_cohortId <- paste(cohortId, collapse = ", ")
+    cli::cat_bullet("Building prevalence for cohort ids:\n   ",crayon::green(cat_cohortId),
+                    bullet = "info", bullet_col = "blue")
+    cat_cohortId <- paste(covId, collapse = ", ")
+    cli::cat_bullet("Using cohorts ids:\n   ", crayon::green(cat_cohortId),
+                    bullet = "info", bullet_col = "blue")
+
+    # Run post-index s
+    cohortCovariates(con = con,
+                     cohortDatabaseSchema = workDatabaseSchema,
+                     cohortTable = cohortTable,
+                     cohortKey = cohortKey,
+                     covariateKey = covariateKey,
+                     timeA = timeA[i],
+                     timeB = timeB[i],
+                     outputFolder = piduFolder)
+
+
+  }
+  tok <- Sys.time()
+  cli::cat_bullet("Execution Completed at: ", crayon::red(tok),
+                  bullet = "info", bullet_col = "blue")
+  tdif <- tok - tik
+  tok_format <- paste(scales::label_number(0.01)(as.numeric(tdif)), attr(tdif, "units"))
+  cli::cat_bullet("Execution took: ", crayon::red(tok_format),
+                  bullet = "info", bullet_col = "blue")
+  invisible(tok)
+
+}
+
+
 ## Treatment Patterns -------------------------
 
 prepSankey <- function(th, minNumPatterns) {
@@ -131,7 +314,7 @@ executeTreatmentPatterns <- function(con,
       fs::dir_create()
 
     #save file
-    file_name <- paste("sankey", targetCohortId, sep = "_")
+    file_name <- paste("sankey", targetCohortId[i], sep = "_")
     save_path2 <- fs::path(save_path, file_name, ext = "rds")
     readr::write_rds(patterns, file = save_path2)
     cli::cat_line()
@@ -250,43 +433,51 @@ executeTimeToEvent <- function(con,
   # list all treatment history files
   thFiles <- fs::dir_ls(thHistoryFolder, recurse = TRUE, type = "file")
 
-  #read parquet file - get treatment history
-  th <- arrow::read_parquet(file = thFiles)
+  for (i in seq_along(thFiles)) {
 
-  #get the target cohort name
-  file_label <- tools::file_path_sans_ext(basename(thFiles)) %>%
-    gsub("th_", "", .)
+    #read parquet file - get treatment history
+    th <- arrow::read_parquet(file = thFiles[i])
 
-
-  #do consoule print
-  cli::cat_line()
-  cli::cat_bullet(crayon::magenta("Execute time to discontinuation for Id: "),
-                  targetCohortId, " (name: ",  targetCohortName, ")",
-                  bullet = "pointer", bullet_col = "yellow")
+    #get the target cohort name
+    file_label <- tools::file_path_sans_ext(basename(thFiles[i])) %>%
+      gsub("th_", "", .)
 
 
-  # get time to event dataframe
-  tteDat <- prepTte(con = con,
-                    th = th,
-                    workDatabaseSchema = workDatabaseSchema,
-                    cohortTable = cohortTable,
-                    targetCohorts = targetCohorts)
+    #do consoule print
+    cli::cat_line()
+    cli::cat_bullet(crayon::magenta("Execute time to discontinuation for Id: "),
+                    targetCohortId[i], " (name: ",  targetCohortName[i], ")",
+                    bullet = "pointer", bullet_col = "yellow")
 
 
-  # create tte folder
-  save_path <- fs::path(tteFolder) %>%
-    fs::dir_create()
+    tmp <- targetCohorts %>%
+      dplyr::filter(
+        id == targetCohortId[i]
+      )
 
-  #save time to event data
-  file_name <- paste(file_label, "tte", sep = "_")
-  save_path2 <- fs::path(save_path, file_name, ext = "csv")
-  readr::write_csv(tteDat, file = save_path2)
-  cli::cat_line()
-  cli::cat_bullet("Saved file ", crayon::green(basename(save_path2)), " to:",
-                  bullet = "info", bullet_col = "blue")
-  cli::cat_bullet(crayon::cyan(save_path), bullet = "pointer", bullet_col = "yellow")
-  cli::cat_line()
+    # get time to event dataframe
+    tteDat <- prepTte(con = con,
+                      th = th,
+                      workDatabaseSchema = workDatabaseSchema,
+                      cohortTable = cohortTable,
+                      targetCohorts = tmp)
 
+
+    # create tte folder
+    save_path <- fs::path(tteFolder) %>%
+      fs::dir_create()
+
+    #save time to event data
+    file_name <- paste("tte", file_label, sep = "_")
+    save_path2 <- fs::path(save_path, file_name, ext = "csv")
+    readr::write_csv(tteDat, file = save_path2)
+    cli::cat_line()
+    cli::cat_bullet("Saved file ", crayon::green(basename(save_path2)), " to:",
+                    bullet = "info", bullet_col = "blue")
+    cli::cat_bullet(crayon::cyan(save_path), bullet = "pointer", bullet_col = "yellow")
+    cli::cat_line()
+
+  }
 
   invisible(tteDat)
 
