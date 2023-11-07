8949d306522db357d3e5f009530b303442c0adaf
Martin Lavallee <44976592+mdlavallee92@users.noreply.github.com>
2023-11-07 11:34:02 -0500

    separate shiny into own folder

--- shiny/app.R
+++ shiny/app.R
@@ -20,7 +20,33 @@ options(shiny.fullstacktrace = FALSE)
 # Variables ---------------------
 title <- "EHDEN HMB"
 
-description <- "The EHDEN HMB study......"
+description <- "
+
+The EHDEN HMB study aims to answer the following questions:
+
+- What are the characteristics of women diagnosed with HMB in terms of demographics, comorbidities, procedures, and comedication?
+
+- What are the treatment pathways of women diagnosed with HMB?
+
+- What is the incidence of HMB across different countries and data sources?
+
+
+
+The primary objectives to answer them are:
+
+1. To characterise women of reproductive age diagnosed with a diagnosis of HMB (demographics, parity and reproductive history, comorbidities, comedications and procedures). [see Clinical Characteristics and Underlying Conditions tabs]
+
+2. To describe treatment pathways of women of reproductive age diagnosed with HMB across different countries and data sources (duration of use, switch to other/s treatment). [see Treatment Patterns tab]
+
+
+
+The exploratory objectives of this study are to:
+
+3. To describe the incidence and proportion of women diagnosed with HMB (calendar year over study period: 2000-2020). [see Incidence tab]
+
+4. To describe country-specific guideline compliant treatment use where national treatment guidelines are available for HMB. [see Treatment Patterns tab]
+
+"
 incidenceDescription <- "Incidence rate is calculated by 'Outcome Count'/'Person Days' * 100."
 underlyingDescription <- "Counts equal to 5 and below have been masked and replaced with '<5'."
 drugUtilizationDescription <- "Drug utilization counts equal to 5 and below have been masked and replaced with '<5."
